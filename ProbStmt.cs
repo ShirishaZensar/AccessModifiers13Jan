43cfc5f64d0f45995bd7b5c9aa61b1ddf61bac98
Siri.G <gangarapu.shirisha@zensar.com>
2021-01-30 23:11:05 +0530

    Add Assignment on Access modifier

---
+++ ProbStmt.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work12Jan
+{
+    //Base Class
+    public class Person2
+    {
+        string name, ssNo;
+        public Person2(string _name, string _ssNo)
+        {
+            name = _name;
+            ssNo = _ssNo;
+        }
+        public virtual void GetInfo()
+        {
+            
+            Console.WriteLine($"Name is : {name}");
+            Console.WriteLine($"SSNO is : {ssNo}");
+        }
+
+    }
+        //Derived class
+        class Employee2 : Person2
+        {
+            string empId;
+            public Employee2(string name, string ssNo, string empId) : base(name, ssNo)
+            {
+                this.empId = empId;
+            }
+            public override void GetInfo()
+            {
+                base.GetInfo();
+                Console.WriteLine($"EmpId is : {empId}");
+            }
+        }
+        class ProbStmt
+        {
+            static void Main()
+            {
+                Console.WriteLine("\n*******\n");
+                Employee2 e = new Employee2("Siri", "54j85", "1214");
+                Console.WriteLine("Details");
+                e.GetInfo();
+            }
+        }
+    }
+
+   
+   
---
+++ ProveModifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work12Jan
+{
+    class Shapes
+    {
+        protected int Radius = 10;
+        protected internal double AreaShape = 254.58;
+        private protected string NameShape = "Siri";
+    }
+    class Circle:Shapes
+    {
+        static void Main()
+        {
+            Circle c1 = new Circle();
+            Console.WriteLine(c1.AreaShape);
+           Console.WriteLine($" Possible:{c1.Radius} and {c1.NameShape}");
+        }
+    }
+}
